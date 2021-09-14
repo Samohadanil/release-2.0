ab800d8061125f8be267166bd7e055dc1ad2cd63
Samoyl <samouylenko@gmail.com>
2021-09-15 01:37:13 +0300

    add fake data in DB

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,13 +16,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $name = array("Anton", "Andrei", "Artur", "Boris", "Vasily", "Danila", "Yakov,", "Sergey", "Artem", "Olga", "Alena", "Dmitriy", "Maria", "Liza");
         for($i=0; $i < 50; $i++) {
             DB::table('donations')->insert(
                 [
-                    'name' => Str::random(40),
+                    'name' => $name[rand(0, 13)],
                     'email' => Str::random(10) . '@gmail.com',
                     'message' => Str::random(50),
-                    'donation' => rand(0, 500),
+                    'donation' => rand(0, 1000),
                     'created_at' => Carbon::now(),
                     'updated_at' => Carbon::now(),
                 ]
